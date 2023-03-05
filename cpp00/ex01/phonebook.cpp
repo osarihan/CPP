d3dba7db00ea5aa532dc748321c53c226b46390e
Oguz Sarihan <osarihan@k2m16s03.42kocaeli.com.tr>
2023-03-05 18:01:57 +0300

    cpp00

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -6,10 +6,125 @@
 /*   By: osarihan <osarihan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:53:36 by osarihan          #+#    #+#             */
-/*   Updated: 2023/03/02 13:01:30 by osarihan         ###   ########.fr       */
+/*   Updated: 2023/03/05 17:58:10 by osarihan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
+void printContact(Contact c, int i) {
+    std::cout << std::right << std::setw(10) << std::setfill(' ') << (i + 1);
+    std::cout << "|";
+    std::cout << std::right << std::setw(10) << std::setfill(' ') << (c.name.length() > 10 ? c.name.substr(0, 9) + "." : c.name);
+    std::cout << "|";
+    std::cout << std::right << std::setw(10) << std::setfill(' ') << (c.surname.length() > 10 ? c.surname.substr(0, 9) + "." : c.surname);
+    std::cout << "|";
+    std::cout << std::right << std::setw(10) << std::setfill(' ') << (c.nickname.length() > 10 ? c.nickname.substr(0, 9) + "." : c.nickname);
+  	std::cout << "|";
+    std::cout << std::endl;
+    std::cout << std::setfill('-') << std::setw(44) << "" << std::endl;
+}
 
+int ft_add(std::string str, Contact *cnt, int i){
+	std::cout << "first name:";
+	getline(std::cin, str);
+	if (str.empty())
+		return(0);
+	cnt[i].name = str;
+	std::cout << std::endl;
+	//
+	std::cout << "surname:";
+	getline(std::cin, str);
+	if (str.empty())
+		return(0);
+	cnt[i].surname = str;
+	std::cout << std::endl;	
+	//
+	std::cout << "nickname:";
+	getline(std::cin, str);
+	if (str.empty())
+		return(0);
+	cnt[i].nickname = str;
+	std::cout << std::endl;
+	//
+	std::cout << "phone number:";
+	getline(std::cin, str);
+	if (str.empty())
+		return(0);
+	cnt[i].number = str;
+	std::cout << std::endl;
+	//
+	std::cout << "darkest secret:";
+	getline(std::cin, str);
+	if (str.empty())
+		return(0);
+	cnt[i].secret = str;
+	std::cout << std::endl;
+	//
+	return (1);
+}
+
+void	ft_clear(Contact *c, int i){
+	c[i].name.erase();
+	c[i].secret.erase();
+	c[i].number.erase();
+	c[i].secret.erase();
+	c[i].surname.erase();
+	return;
+}
+
+void	ft_print(Contact c, int i){
+	std::cout << c.name << std::endl;
+	std::cout << c.surname << std::endl;
+	std::cout << c.nickname << std::endl;
+	std::cout << c.number << std::endl;
+	std::cout << c.secret << std::endl;
+	return;
+}
+
+int main(void){
+	int	i = 0;
+	PhoneBook book;
+	std::string str;
+	std::string search;
+	int numbe;
+
+	while (1){
+		if (i == 8)
+			i = 0;
+		std::cout << "Please type your command" << std::endl;
+		std::cout << "==>>";
+		getline(std::cin, str);
+		if (str == "ADD"){
+			if (ft_add(str, book.list, i) == 1){
+				std::cout << "creation successful" << std::endl;
+				i++;
+			}
+			else{
+				std::cout << "fields cannot be empty" << std::endl;
+				ft_clear(book.list, i);
+			}
+		}
+		else if (str == "SEARCH"){
+			for (int i=0; i < 8; i++){
+				printContact(book.list[i], i);
+			}
+			std::cout << "Please type the number of contact you're trying to search" << std::endl;
+			getline(std::cin, search);
+			numbe = atoi(search.c_str());
+			if (numbe > 7)
+				std::cout << "Error" << std::endl;	
+			else
+				ft_print(book.list[i - 1], i - 1);
+		}
+		else if (str == "EXIT"){
+			for (int i=0; i < 8; i++){
+				ft_clear(book.list, i);
+			}
+			exit(0);
+		}
+		else{
+			std::cout << "invalid command please try again" << std::endl;
+		}
+	}
+}
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: osarihan <osarihan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:55:01 by osarihan          #+#    #+#             */
-/*   Updated: 2023/03/04 14:06:35 by osarihan         ###   ########.fr       */
+/*   Updated: 2023/03/05 17:59:17 by osarihan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,29 @@
 #define PHONEBOOK_HPP
 
 #include <iostream>
+#include <string>
+#include <iomanip> 
+#include <unistd.h>
 
 class Contact{
-	public:
-		std::string name;
-    	std::string surname;
-    	std::string nickname;
-    	std::string secret;
-    	std::string number;
+public:
+	std::string name;
+	std::string surname;
+	std::string nickname;
+	std::string secret;
+	std::string number;
 };
 
 class PhoneBook{
-	public:
-		contact list[8];
-		int i;
+public:
+	Contact list[8];
+	int i;
 		
 };
+
+void 	printContact(Contact c, int i);
+int 	ft_add(std::string str, Contact *cnt, int i);
+void	ft_clear(Contact *c, int i);
+void	ft_print(Contact c, int i);
 
 #endif
