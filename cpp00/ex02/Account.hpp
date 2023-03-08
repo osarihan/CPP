1b80101fac2140b94078dc39d66746018b3a4d6b
Oguz Sarihan <osarihan@k2m17s02.42kocaeli.com.tr>
2023-03-08 15:39:22 +0300

    ex03

--- cpp00/ex02/Account.hpp
+++ cpp00/ex02/Account.hpp
@@ -23,10 +23,10 @@ public:
 
 	typedef Account		t;
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
+	static int	getNbAccounts( void ); //
+	static int	getTotalAmount( void );//
+	static int	getNbDeposits( void );//
+	static int	getNbWithdrawals( void );//
 	static void	displayAccountsInfos( void );//
 
 	Account( int initial_deposit );//
@@ -35,7 +35,7 @@ public:
 	void	makeDeposit( int deposit );//
 	bool	makeWithdrawal( int withdrawal );//
 	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;//
+	void	displayStatus( void ) const;
 
 
 private:
